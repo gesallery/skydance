a3d5a84e45ed7f8fe42b7a0ca3277dee3b2f491f
gsallery <gavin@sallery.co.uk>
2022-01-29 15:16:00 +0000

    Tidy up logging, and replace sequence generation with truncated millisecond timestamp

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 import net from 'net';
 
-console.log('running');
-const socket = net.Socket();
-let seq = 0;
-
-const incSeq = () => {
-  seq = seq + 1;
-  if (seq > 255) seq = 0;
-  return seq;
-};
+const PORT = '8899';
 
 export const zonePower = (zone, onOff) => {
-  console.log('Connection made');
-  const dataWritten = () => console.log('Data written');
+    const prefixBuffer = Buffer.from('55AA5AA57E', 'hex');
+    const seqBuf = Buffer.allocUnsafe(1);
+    // seqBuf.writeUInt8(incSeq);
+    seqBuf.writeUInt8(new Date().getTime() % 255);
+    const fixedBuf = Buffer.from('8011803071537B', 'hex');
+    const zoneBuf = Buffer.allocUnsafe(1);
+    zoneBuf.writeUInt8(zone);
+    const functionBuf = Buffer.from('000A0100', 'hex');
+    const onOffBuf = Buffer.allocUnsafe(1);
+    onOffBuf.writeUInt8(onOff);
+    const postfixBuffer = Buffer.from('007E', 'hex');
 
-  const prefixBuffer = Buffer.from('55AA5AA57E', 'hex');
-  const seqBuf = Buffer.allocUnsafe(1);
-  seqBuf.writeUInt8(incSeq);
-  const fixedBuf = Buffer.from('8011803071537B', 'hex');
-  const zoneBuf = Buffer.allocUnsafe(1);
-  zoneBuf.writeUInt8(zone);
-  const functionBuf = Buffer.from('000A0100', 'hex');
-  const onOffBuf = Buffer.allocUnsafe(1);
-  onOffBuf.writeUInt8(onOff);
-  const postfixBuffer = Buffer.from('007E', 'hex');
+    const socket = net.Socket();
+    socket.setNoDelay(true);
 
-  socket.write(
-    Buffer.concat([
-      prefixBuffer,
-      seqBuf,
-      fixedBuf,
-      zoneBuf,
-      functionBuf,
-      onOffBuf,
-      postfixBuffer,
-    ]),
-    dataWritten
-  );
+    // socket.on('data', (data) => console.log(data));
+
+    socket.connect(PORT, '192.168.1.159', () => {
+        socket.write(
+            Buffer.concat([
+                prefixBuffer,
+                seqBuf,
+                fixedBuf,
+                zoneBuf,
+                functionBuf,
+                onOffBuf,
+                postfixBuffer,
+            ]),
+            () => {
+                socket.end();
+            }
+        );
+    });
 };
 
-socket.on('data', (data) => console.log(data));
+if (process.argv.length > 2) {
+    const args = process.argv.slice(2);
 
-socket.connect('8899', '192.168.1.159', () => {
-  zonePower(4, 1);
-});
+    zonePower(args[0], args[1]);
+}
